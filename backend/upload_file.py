fd4bdf740d84a3c0596bade9868834f86168b92a
cde-ksh <itsmekshka@gmail.com>
2026-08-30 15:28:42 +0530

    v2 features(destination)

--- backend/upload_file.py
+++ backend/upload_file.py
@@ -1,8 +1,18 @@
 from pathlib import Path
+
+BASE_DIR = Path("/Users/kshiraj/Desktop/LibManage/backend/filemanager")
+
 def get_file():
     file = Path(input("Import your file: "))
-    if not file.exists():
+    if not file.is_file():
         raise FileNotFoundError("File not exists...")
     
     return file
 
+def get_destination():
+    dest = input("Enter your destination: ")
+    destination = BASE_DIR / dest
+    if not destination.exists():
+        destination.mkdir(parents=True, exist_ok=True)
+
+    return destination
